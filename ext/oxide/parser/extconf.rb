3705842504d62a8bd512f44cc284dd329a30099f
Richard E. Dodson <richard.elias.dodson@gmail.com>
2025-04-02 20:45:28 -0500

    Define LEX in Makefile

--- ext/oxide/parser/extconf.rb
+++ ext/oxide/parser/extconf.rb
@@ -6,6 +6,7 @@ SRC_EXT << %w[l y]
 
 create_makefile('parser') do |config|
   config << <<~MAKEFILE.gsub('  ', "\t")
+    LEX  := PATH="$${HOMEBREW_PREFIX}/opt/flex/bin:$${PATH}"  flex
     YACC := PATH="$${HOMEBREW_PREFIX}/opt/bison/bin:$${PATH}" bison
 
     .DEFAULT_GOAL := all
